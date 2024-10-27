b386e4b22b289153f6e000b0b23045d2fb12e02f
Alex Doehring <alex.doehring2@gmail.com>
2024-10-27 16:35:58 -0500

    changed roots to routes.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from .roots import expenses_bp
+from .routes import expenses_bp
 from .auth import auth_bp
 from flask_login import LoginManager
 from .models import User 
