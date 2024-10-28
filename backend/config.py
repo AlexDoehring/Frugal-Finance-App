029f3eea85bedca33c4d41b1380de62fc93b0e89
Alex Doehring <alex.doehring2@gmail.com>
2024-10-27 20:40:45 -0500

    updated secret key

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 # config.py
 import os
 class Config: 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'SECRET_KEY' 
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
