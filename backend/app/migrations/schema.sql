5e7cba97ae14b9fcce2e6c5e1eb485bb36d38491
kylespragg <kylesspragg@gmail.com>
2024-11-18 20:08:03 -0600

    Updated schema file, created updated table 'budget' in app.db

--- backend/app/migrations/schema.sql
+++ backend/app/migrations/schema.sql
@@ -1,31 +1,31 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(200) NOT NULL
 );
 
-CREATE TABLE expense (
+CREATE TABLE IF NOT EXISTS expense (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     amount FLOAT NOT NULL,
     category VARCHAR(50) NOT NULL,
     date DATE NOT NULL,
-    description VARCHAR(255) --optional
+    description VARCHAR(255), --optional
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE budget (
+CREATE TABLE IF NOT EXISTS budget (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     amount FLOAT NOT NULL,
     threshold FLOAT NOT NULL,
     category VARCHAR(50) NOT NULL,
-    description VARCHAR(255) --optional
+    description VARCHAR(255), --optional
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
-CREATE TABLE income (
+CREATE TABLE IF NOT EXISTS income (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     source_name VARCHAR(100) NOT NULL,
